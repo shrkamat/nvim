b067fa5820aae159bb2e1ffb17f4917d182e625a
Shrinivas Kamath <skamath@synamedia.com>
2021-12-25 23:14:00 +0530

    nvim lua
    neovim from scratch -
    https://www.youtube.com/watch?v=ctH-a-1eUME&list=PLhoH5vyxr6Qq41NFL4GvhFp-WLd5xzIzZ

---
+++ init.lua
@@ -0,0 +1,18 @@
+require("options")
+require("plugins")
+require("completion")
+require("lsp")
+require("tscope")
+require("keymaps")    -- maintain this last ?
+
+
+vim.cmd "colorscheme gruvbox-material"
+vim.cmd "highlight HighlightedyankRegion cterm=reverse gui=reverse"       -- updated highlightedyank color
+vim.cmd "set rnu"
+vim.cmd "set nu"
+vim.cmd "set cursorline"
+vim.cmd "set encoding=UTF-8"
+vim.cmd "set nocompatible"
+
+-- simple setup(s) that won't have any customizations
+require("gitsigns").setup()
