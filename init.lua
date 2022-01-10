663ab87c05f3e1103b410b751b5c55378461a483
Shrinivas Kamath <skamath@synamedia.com>
2022-01-10 11:13:23 +0530

    toggleterm

--- init.lua
+++ init.lua
@@ -3,13 +3,12 @@ require("plugins")
 require("completion")
 require("lsp")
 require("tscope")
+require("config.toggleterm")
 require("keymaps")    -- maintain this last ?
 
 
 vim.cmd "colorscheme gruvbox-material"
 vim.cmd "highlight HighlightedyankRegion cterm=reverse gui=reverse"       -- updated highlightedyank color
--- vim.cmd "set rnu"
--- vim.cmd "set nu"
 vim.cmd "set cursorline"
 vim.cmd "set encoding=UTF-8"
 vim.cmd "set nocompatible"
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -89,6 +89,9 @@ return packer.startup(function(use)
      -- bufferline
      use "akinsho/bufferline.nvim"
 
+     -- terminal handling
+     use {"akinsho/toggleterm.nvim"}
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
