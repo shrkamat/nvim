28b0699797e6582d0f749a145f133699f9d717e0
Shrinivas Kamath <skamath@synamedia.com>
2023-03-03 23:44:43 +0530

    F1 maps to help tags

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -41,9 +41,9 @@ packer.init {
 -- Install your plugins here
 return packer.startup(function(use)
     -- My plugins here
-    use "wbthomason/packer.nvim"  -- Have packer manage itself
-    use "nvim-lua/popup.nvim"     -- An implementation of the Popup API from vim in Neovim
-    use "nvim-lua/plenary.nvim"   -- Useful lua functions used ny lots of plugins
+    use "wbthomason/packer.nvim" -- Have packer manage itself
+    use "nvim-lua/popup.nvim"    -- An implementation of the Popup API from vim in Neovim
+    use "nvim-lua/plenary.nvim"  -- Useful lua functions used ny lots of plugins
     use "sainnhe/gruvbox-material"
     use "machakann/vim-highlightedyank"
 
@@ -53,22 +53,22 @@ return packer.startup(function(use)
         branch = 'v1.x',
         requires = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' },              -- Required
-            { 'williamboman/mason.nvim' },            -- Optional
-            { 'williamboman/mason-lspconfig.nvim' },  -- Optional
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },          -- Required
-            { 'hrsh7th/cmp-nvim-lsp' },      -- Required
-            { 'hrsh7th/cmp-buffer' },        -- Optional
-            { 'hrsh7th/cmp-path' },          -- Optional
-            { 'saadparwaiz1/cmp_luasnip' },  -- Optional
-            { 'hrsh7th/cmp-nvim-lua' },      -- Optional
+            { 'hrsh7th/nvim-cmp' },         -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+            { 'hrsh7th/cmp-buffer' },       -- Optional
+            { 'hrsh7th/cmp-path' },         -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
             -- Snippets
-            { 'L3MON4D3/LuaSnip' },                 -- Required
-            { 'rafamadriz/friendly-snippets' },     -- Optional
-            { 'benfowler/telescope-luasnip.nvim' }  -- TODO recheck
+            { 'L3MON4D3/LuaSnip' },                -- Required
+            { 'rafamadriz/friendly-snippets' },    -- Optional
+            { 'benfowler/telescope-luasnip.nvim' } -- TODO recheck
         }
     }
 
