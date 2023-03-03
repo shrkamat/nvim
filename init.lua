504205954bde9bbdf5f3572edb6dab9a41fee3f1
Shrinivas Kamath <skamath@synamedia.com>
2023-03-04 01:10:08 +0530

    few custom commands
    - FFTelescope       -- regular telescope find files
    - FFCodeSearch      -- find files via code search index
    - ResSearch         -- resume search aka `Telescope resume`

--- init.lua
+++ init.lua
@@ -79,7 +79,7 @@ require("flutter-tools").setup {
         }
     },
     flutter_lookup_cmd = nil, -- example "dirname $(which flutter)" or "asdf where flutter"
-    fvm = false,            -- takes priority over path, uses <workspace>/.fvm/flutter_sdk if enabled
+    fvm = false,              -- takes priority over path, uses <workspace>/.fvm/flutter_sdk if enabled
     widget_guides = {
         enabled = false,
     },
@@ -93,17 +93,17 @@ require("flutter-tools").setup {
         open_cmd = "tabedit", -- command to use to open the log buffer
     },
     dev_tools = {
-        autostart = false,     -- autostart devtools server if not detected
+        autostart = false,         -- autostart devtools server if not detected
         auto_open_browser = false, -- Automatically opens devtools in the browser
     },
     outline = {
         open_cmd = "20vnew", -- command to use to open the outline buffer
-        auto_open = false -- if true this will open the outline automatically when it is first populated
+        auto_open = false    -- if true this will open the outline automatically when it is first populated
     },
     lsp = {
         color = {
-                          -- show the derived colours for dart variables
-            enabled = false, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+            -- show the derived colours for dart variables
+            enabled = false,    -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
             background = false, -- highlight the background
             foreground = false, -- highlight the foreground
             -- virtual_text = true, -- show the highlight using virtual text
@@ -135,3 +135,21 @@ easypick.setup({
         },
     }
 })
+
+vim.api.nvim_create_user_command(
+    "ResSearch",
+    ":Telescope resume",
+    {}
+)
+
+vim.api.nvim_create_user_command(
+    "FFCodeSearch",
+    ':lua require("telescope.builtin").find_files({ find_command = { "csearch", "-l", "." } })',
+    {}
+)
+
+vim.api.nvim_create_user_command(
+    "FFTelescope",
+    ':Telescope find_files',
+    {}
+)
